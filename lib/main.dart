056ccdee5a3fec78cbaf4714e50a2fddd8080d9d
枫叶砂 <1724478441@qq.com>
2020-06-04 16:27:37 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -326,189 +326,202 @@
 //     }
 //   }
 // }
+import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:isolate';
 
-// import 'dart:convert';
-// import 'dart:isolate';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+void main() {
+  runApp(SampleApp());
+}
+
+class SampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  showLoadingDialog() {
+    if (widgets.length == 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  getBody() {
+    if (showLoadingDialog()) {
+      return getProgressDialog();
+    } else {
+      return getListView();
+    }
+  }
+
+  getProgressDialog() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Sample App"),
+        ),
+        onPressed: (){
+          print("object");
+        },
+        body: 
+        // getBody(),
+        // 滚动条
+        Theme.of(context).platform == TargetPlatform.iOS
+          ? CupertinoScrollbar(
+              child: getBody(),
+            )
+          : Scrollbar(
+              child: getBody(),
+            ),
+            );
+  }
+
+  ListView getListView() => ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (BuildContext context, int position) {
+        return getRow(position);
+      });
+
+  Widget getRow(int i) {
+    return Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text("Row ${widgets[i]["title"]}"),
+        );
+  }
+
+  loadData() async {
+    // 打开ReceivePort以接收传入的消息
+    ReceivePort receivePort = ReceivePort();
+    //创建并生成与当前Isolate共享相同代码的Isolate
+    await Isolate.spawn(dataLoader, receivePort.sendPort);
+
+    // 流的第一个元素
+    SendPort sendPort = await receivePort.first;
+    // 流的第一个元素被收到后监听会关闭，所以需要新打开一个ReceivePort以接收传入的消息
+    ReceivePort response = ReceivePort();
+    //通过此发送端口向其对应的“ReceivePort”①发送异步[消息]，这个“消息”指的是发送的参数②。
+    sendPort.send(
+        ["https://jsonplaceholder.typicode.com/posts", response.sendPort]);
+    // 获取端口发送来的数据③
+    List msg = await response.first;
+
+    setState(() {
+      widgets = msg;
+    });
+  }
+
+  // isolate的入口函数，该函数会在新的Isolate中调用，Isolate.spawn的message参数会作为调用它时的唯一参数
+  static dataLoader(SendPort sendPort) async {
+    // 打开ReceivePort①以接收传入的消息
+    ReceivePort port = ReceivePort();
+
+    // 通知其他的isolates，本isolate 所监听的端口
+    sendPort.send(port.sendPort);
+    // 获取其他端口发送的异步消息 msg② -> ["https://jsonplaceholder.typicode.com/posts", response.sendPort]
+    await for (var msg in port) {
+      //等价于List msg= await port.first;
+      String data = msg[0];
+      SendPort replyTo = msg[1];
+
+      String dataURL = data;
+      http.Response response = await http.get(dataURL);
+      // 其对应的“ReceivePort”发送解析出来的JSON数据③
+      replyTo.send(json.decode(response.body));
+    }
+  }
+}
+
+// import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
 
-// void main() {
-//   runApp(SampleApp());
-// }
+// void main() => runApp(new MyApp());
 
-// class SampleApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       title: 'Sample App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: SampleAppPage(),
+//       home: MyHomePage(),
 //     );
 //   }
 // }
 
-// class SampleAppPage extends StatefulWidget {
-//   SampleAppPage({Key key}) : super(key: key);
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({
+//     Key key,
+//   }) : super(key: key);
 
 //   @override
-//   _SampleAppPageState createState() => _SampleAppPageState();
+//   _MyHomePageState createState() => _MyHomePageState();
 // }
 
-// class _SampleAppPageState extends State<SampleAppPage> {
-//   List widgets = [];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadData();
-//   }
-
-//   showLoadingDialog() {
-//     if (widgets.length == 0) {
-//       return true;
-//     }
-
-//     return false;
-//   }
-
-//   getBody() {
-//     if (showLoadingDialog()) {
-//       return getProgressDialog();
-//     } else {
-//       return getListView();
-//     }
-//   }
-
-//   getProgressDialog() {
-//     return Center(child: CircularProgressIndicator());
-//   }
-
+// class _MyHomePageState extends State<MyHomePage> {
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//         appBar: AppBar(
-//           title: Text("Sample App"),
-//         ),
-//         body: getBody());
+//       appBar: AppBar(
+//         title: Text('demo'),
+//       ),
+//       body: Theme.of(context).platform == TargetPlatform.iOS
+//           ? CupertinoScrollbar(
+//               child: _buildListView(),
+//             )
+//           : Scrollbar(
+//               child: _buildListView(),
+//             ),
+//     );
 //   }
 
-//   ListView getListView() => ListView.builder(
-//       itemCount: widgets.length,
-//       itemBuilder: (BuildContext context, int position) {
-//         return getRow(position);
-//       });
-
-//   Widget getRow(int i) {
+//   Padding _buildListView() {
 //     return Padding(
-//         padding: EdgeInsets.all(10.0),
-//         child: Text("Row ${widgets[i]["title"]}"),
-//         );
-//   }
-
-//   loadData() async {
-//     // 打开ReceivePort以接收传入的消息
-//     ReceivePort receivePort = ReceivePort();
-//     //创建并生成与当前Isolate共享相同代码的Isolate
-//     await Isolate.spawn(dataLoader, receivePort.sendPort);
-
-//     // 流的第一个元素
-//     SendPort sendPort = await receivePort.first;
-//     // 流的第一个元素被收到后监听会关闭，所以需要新打开一个ReceivePort以接收传入的消息
-//     ReceivePort response = ReceivePort();
-//     //通过此发送端口向其对应的“ReceivePort”①发送异步[消息]，这个“消息”指的是发送的参数②。
-//     sendPort.send(
-//         ["https://jsonplaceholder.typicode.com/posts", response.sendPort]);
-//     // 获取端口发送来的数据③
-//     List msg = await response.first;
-
-//     setState(() {
-//       widgets = msg;
-//     });
-//   }
-
-//   // isolate的入口函数，该函数会在新的Isolate中调用，Isolate.spawn的message参数会作为调用它时的唯一参数
-//   static dataLoader(SendPort sendPort) async {
-//     // 打开ReceivePort①以接收传入的消息
-//     ReceivePort port = ReceivePort();
-
-//     // 通知其他的isolates，本isolate 所监听的端口
-//     sendPort.send(port.sendPort);
-//     // 获取其他端口发送的异步消息 msg② -> ["https://jsonplaceholder.typicode.com/posts", response.sendPort]
-//     await for (var msg in port) {
-//       //等价于List msg= await port.first;
-//       String data = msg[0];
-//       SendPort replyTo = msg[1];
-
-//       String dataURL = data;
-//       http.Response response = await http.get(dataURL);
-//       // 其对应的“ReceivePort”发送解析出来的JSON数据③
-//       replyTo.send(json.decode(response.body));
-//     }
+//       padding: const EdgeInsets.all(8.0),
+//       child: SingleChildScrollView(
+//         child: Column(children: generatorLists(30)),
+//       ),
+//     );
 //   }
 // }
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('demo'),
-      ),
-      body: Theme.of(context).platform == TargetPlatform.iOS
-          ? CupertinoScrollbar(
-              child: _buildListView(),
-            )
-          : Scrollbar(
-              child: _buildListView(),
-            ),
-    );
-  }
-
-  Padding _buildListView() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SingleChildScrollView(
-        child: Column(children: generatorLists(30)),
-      ),
-    );
-  }
-}
-
-List<Widget> generatorLists(n) {
-  List<Widget> lst = [];
-  for (var i = 0; i < n; i++) {
-    lst.add(Container(
-      decoration: BoxDecoration(color: Colors.grey[400]),
-      width: double.infinity,
-      height: 30,
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-    ));
-  }
-  return lst;
-}
+// List<Widget> generatorLists(n) {
+//   List<Widget> lst = [];
+//   for (var i = 0; i < n; i++) {
+//     lst.add(Container(
+//       decoration: BoxDecoration(color: Colors.grey[400]),
+//       width: double.infinity,
+//       height: 30,
+//       margin: EdgeInsets.symmetric(vertical: 8.0),
+//     ));
+//   }
+//   return lst;
+// }
